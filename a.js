c6b83722e07dc53815c08bbd3383971b0bfc45a7
LUOXI <luox62620@gmail.com>
2026-04-21 18:10:47 +0800

    Fix/a (#3) 修复了a模块
    * feat: 新增a模块的函数实现
    
    * fix: 新增a模块函数导出
    
    ---------
    
    Co-authored-by: lllluoxi <53041755+LLLLuoXi@users.noreply.github.com>

--- a.js
+++ a.js
@@ -1,10 +1,9 @@
+function a() {}
 
-function a(){}
-
-function fn1(...args){
-  console.log(...args)
+export function fn1(...args) {
+  console.log('fn1', ...args)
 }
 
-function fn2(...args){
-  console.log(...args)
+export function fn2(...args) {
+  console.log('fn2', ...args)
 }
