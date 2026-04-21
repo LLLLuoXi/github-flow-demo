80647cdc9598d0c0b32baaf9fd5b0e0c503014bc
lllluoxi <53041755+LLLLuoXi@users.noreply.github.com>
2026-04-21 17:40:17 +0800

    fix: 新增a模块函数导出

--- a.js
+++ a.js
@@ -1,13 +1,9 @@
 function a() {}
 
-function fn1() {
-  console.log('fn1')
-}
-
-function fn1(...args) {
+export function fn1(...args) {
   console.log('fn1', ...args)
 }
 
-function fn2(...args) {
+export function fn2(...args) {
   console.log('fn2', ...args)
 }
