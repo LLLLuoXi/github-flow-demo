a49c768a49142496c6c00dc48d5e596ec3cc4eaa
lllluoxi <53041755+LLLLuoXi@users.noreply.github.com>
2026-04-21 17:39:18 +0800

    feat: 新增a模块的函数实现

--- a.js
+++ a.js
@@ -1,10 +1,13 @@
+function a() {}
 
-function a(){}
-
-function fn1(...args){
-  console.log(...args)
+function fn1() {
+  console.log('fn1')
 }
 
-function fn2(...args){
-  console.log(...args)
+function fn1(...args) {
+  console.log('fn1', ...args)
+}
+
+function fn2(...args) {
+  console.log('fn2', ...args)
 }
